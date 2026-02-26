ab702f5af6cb16190f6a2cf3c8335aef645cea69
Shihab Sarar <shihab312417@gmail.com>
2026-02-27 02:26:09 +0600

    Add error handling for division

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply, divide
 
 print("Name: Shihab Sarar")
 print("Today's Date:", date.today())
@@ -7,3 +7,4 @@ print("Today's Date:", date.today())
 print("Add:", add(5, 3))
 print("Subtract:", subtract(10, 4))
 print("Multiply:", multiply(4, 5))
+print("Divide:", divide(10, 0))
