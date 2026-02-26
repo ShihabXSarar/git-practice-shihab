1bd54da7ad2898c77a962bc038b58c3f2f90f3a7
Shihab Sarar <shihab312417@gmail.com>
2026-02-27 02:22:31 +0600

    Add multiply function to calculator

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Name: Shihab Sarar")
 print("Today's Date:", date.today())
 
 print("Add:", add(5, 3))
 print("Subtract:", subtract(10, 4))
+print("Multiply:", multiply(4, 5))
