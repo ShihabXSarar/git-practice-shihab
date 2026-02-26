3b9f0fd0e3ba1031ea3ddc803b6ea261cfd4fb29
Shihab Sarar <shihab312417@gmail.com>
2026-02-27 02:16:30 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from datetime import date
+from utils import add, subtract
 
 print("Name: Shihab Sarar")
 print("Today's Date:", date.today())
+
+print("Add:", add(5, 3))
+print("Subtract:", subtract(10, 4))
